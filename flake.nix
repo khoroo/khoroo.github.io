cd23b93f84c9eece6eeb87cab7fb8df7516e52b7
Robert Sparks <robert.william.sparks@gmail.com>
2024-11-24 16:51:52 +0000

    updated icon

--- flake.nix
+++ flake.nix
@@ -1,20 +1,22 @@
 {
   description = "A flake for developing and building a Zola site";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    swatches = {
+      url = "github:khoroo/swatches";
+      flake = false;
+    };
     zola-theme = {
       url = "github:khoroo/emily_zola_theme";
       flake = false;
     };
   };
-
-  outputs = { self, nixpkgs, flake-utils, zola-theme }:
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        themeName = ((builtins.fromTOML (builtins.readFile "${zola-theme}/theme.toml")).name);
+        themeName = ((builtins.fromTOML (builtins.readFile "${inputs.zola-theme}/theme.toml")).name);
       in
       {
         packages.website = pkgs.stdenv.mkDerivation rec {
@@ -22,23 +24,39 @@
           version = "2024-11-24";
           src = ./.;
           nativeBuildInputs = [ pkgs.zola ];
-          
+
           configurePhase = ''
-            mkdir -p "themes/${themeName}"
-            cp -r ${zola-theme}/* "themes/${themeName}"
+            # First make everything writable
+            find . -type d -exec chmod 755 {} +
+            find . -type f -exec chmod 644 {} +
+
+            # Set up theme
+            mkdir -p themes/${themeName}
+            cp -r ${inputs.zola-theme}/* themes/${themeName}/
+            chmod -R 755 themes
           '';
-          
-          buildPhase = "zola build";
-          installPhase = "cp -r public $out";
+
+          buildPhase = ''
+            chmod -R 755 .
+            HOME=$TMPDIR zola build
+          '';
+
+          installPhase = ''
+            chmod -R 755 public
+            mkdir -p $out
+            cp -r public/* $out/
+            mkdir -p $out/swatches
+            cp -r ${inputs.swatches}/docs/* $out/swatches/
+          '';
         };
-        
-        defaultPackage = self.packages.${system}.website;
-        
+
+        packages.default = self.packages.${system}.website;
+
         devShell = pkgs.mkShell {
           packages = [ pkgs.zola ];
           shellHook = ''
             mkdir -p themes
-            ln -sn "${zola-theme}" "themes/${themeName}"
+            ln -sn "${inputs.zola-theme}" "themes/${themeName}"
           '';
         };
       }
