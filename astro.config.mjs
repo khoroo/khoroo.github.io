0f01585b053cd39cbd726f9d0410e7c6659dd817
Robert Sparks <robert.william.sparks@gmail.com>
2025-03-15 20:04:48 +0000

    moved to static image from overcomplicated web workers etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  vite: {
-    // Ensure TypeScript is properly processed
-    build: {
-      target: 'esnext',
-    },
-    // Allow direct import of TypeScript modules in client-side scripts
-    optimizeDeps: {
-      include: ['src/scripts/attractor.ts']
-    },
-    worker: {
-      format: 'es', // Use ES modules format for workers
-    },
-    // Ensure .ts extension is properly resolved for workers
-    resolve: {
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
-    }
-  }
 });
